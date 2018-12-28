0d1659254c0badf1d7d118a0e4d22479c847426b
anfractuosity <anfractuosity@anfractuosity.com>
2018-12-28 13:10:54 +0000

    spi

--- fpga lighting.srcs/constrs_1/new/constraints.xdc
+++ fpga lighting.srcs/constrs_1/new/constraints.xdc
@@ -4,5 +4,11 @@ create_clock -add -name sys_clk_pin -period 8.0 [get_ports { CLK_50MHz }];
 
 set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { STRIP1 }]; 
 
+set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { SPI_CLK }]; 
+
+set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports { SPI_DATA_IN }]; 
+
 set_property -dict { PACKAGE_PIN M14 IOSTANDARD LVCMOS33 } [get_ports { LED }]; 
 
+
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets SPI_CLK_IBUF] ;
--- fpga lighting.srcs/sources_1/new/lighting.vhd
+++ fpga lighting.srcs/sources_1/new/lighting.vhd
@@ -7,9 +7,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.MATH_REAL.ALL;
 use IEEE.MATH_REAL;
 
+USE ieee.numeric_std.ALL; 
+
+
+
 entity lighting is
 
 port (
+    SPI_CLK: in std_logic;
+    SPI_DATA_IN: in std_logic;
     CLK_50MHz: in std_logic;
     LED: out std_logic;
     STRIP1: out std_logic
@@ -18,37 +24,112 @@ port (
 end lighting;
 
 architecture Behavioral of lighting is
-    
-    constant t0h : real := 35.0e-6;
-    constant t1h : real := 0.9e-6;
-    constant t0l : real := 0.9e-6;
-    constant t1l : real := 35.0e-6;
-    constant res : real := 50.0e-6;    
-    
-    signal data2: unsigned(23 downto 0) := "111111110000000000000000"; -- each LED is 24 bits, we output the same bitstream to all LEDs
 
+    constant size: integer := 60*5*3;  -- Tracks the length of the high or low
+        
+    type ram1 is array(0 to size*2) of std_logic_vector(0 to 23);
+    signal RAM_1 : ram1 := (others => "000000000000000011111111");
+    
+    --type ram2 is array(0 to 60*5*3-1) of std_logic_vector(23 downto 0);
+    --signal RAM_2 : ram2;
+ 
+    signal data2: std_logic_vector(0 to 23) := "000000000000000000000000"; -- each LED is 24 bits, we output the same bitstream to all LEDs
+
+    
     signal long: integer := 113;
     signal short: integer := 44;
     signal refresh: integer := 6250;
 
-
     signal ct: integer := 0;  -- Tracks the length of the high or low
     signal ct2: integer := 0; -- Keeps track if we are outputting a high
     signal ct3: integer := 0; -- Keeps track if we are outputting a high
-    
     signal pulse: integer := 0; -- Keeps track if we are outputting a high
+    
+    signal req_a: boolean := false; -- Keeps track if we are outputting a high
+    signal req_b: boolean := false; -- Keeps track if we are outputting a high
+
+    signal grant_a: bit := '0'; -- Keeps track if we are outputting a high
+    signal grant_b: bit := '0'; -- Keeps track if we are outputting a high
+
+    signal spi_counter: integer := 0;
+    signal spi_frame_counter: integer := 0;
+
+    signal spi_data: std_logic_vector(0 to 23) := "000000000000000000000000";
+
+    signal off: bit := '0';
+
 
 
     begin
+
+        --LED <= '0';
+        --STRIP1 <= '0';
+
+        
+        Arb: process(req_a, req_b)
+        begin
+            if req_a then 
+                grant_a <= '1'; 
+                grant_b <= '0'; 
+            elsif req_b then 
+                grant_a <= '0'; 
+                grant_b <= '1'; 
+            else 
+                grant_a <= '0'; 
+                grant_b <= '0';
+            end if;
+        end Process Arb;
+        
+    
+        SPI: process(SPI_CLK)
+        begin
+            if rising_edge(SPI_CLK) then
+                
+                if spi_counter = 23 then
+                
+                    if off = '0' then
+                        RAM_1(spi_frame_counter) <= spi_data(0 to 22) & SPI_DATA_IN  ;
+                    else
+                        RAM_1(spi_frame_counter+size) <= spi_data(0 to 22) & SPI_DATA_IN  ;
+                    end if;
+                    
+                    if spi_frame_counter = size then
+                        spi_frame_counter <= 0;
+                        off <= off xor '1';
+                    else
+                        spi_frame_counter <= spi_frame_counter + 1; 
+                    end if;
+                    
+                    spi_data <= "000000000000000000000000";
+                    spi_counter <= 0;
+                    
+                else 
+                        
+                    spi_data(spi_counter) <= SPI_DATA_IN;   
+                    spi_counter <= spi_counter + 1;
+        
+                
+                end if;
+                
+            end if;
+        end Process SPI;
 
         Prescaler: process(CLK_50MHz)
         begin
 
             if rising_edge(CLK_50MHz) then
-            
+
+                
+                if off = '0' then
+                    data2 <= RAM_1(ct3+size) ;
+                else
+                    data2 <= RAM_1(ct3)  ;
+                end if;
+                                  
+                                  
                 ct <= ct + 1;
                 
-                if ct3 = 10 then
+                if ct3 = size then
                     if ct > refresh then
                         ct <= 0;
                         ct3 <= 0;
@@ -56,23 +137,22 @@ architecture Behavioral of lighting is
                         STRIP1 <= '0';                    
                     end if;                
                 else
-                
-                    if data2(ct2) = '0' then
-                        pulse <= short;
-                    else
-                        pulse <= long;
-                    end if;
-                                
+                        
                     if ct > short+long then
-                        ct <= 0;
-                        ct2 <= ct2 + 1;
-                                     
+                        
+                        
                         if ct2 = 23 then
                             ct2 <= 0;
                             ct3 <= ct3 + 1;
+                        else 
+                            ct2 <= ct2 + 1; 
                         end if;
                                      
-                    elsif ct > pulse then
+                        ct <= 0;
+                        
+                    elsif data2(ct2) = '0' and ct > short then
+                        STRIP1 <= '0';
+                    elsif data2(ct2) = '1' and ct > long then
                         STRIP1 <= '0';
                     else
                         STRIP1 <= '1';
