4a5a78731bb9d89538bafdbaac7a0cc5e69486d4
anfractuosity <anfractuosity@anfractuosity.com>
2020-04-13 01:01:00 +0100

    double buffer

--- fpga lighting.srcs/sources_1/new/lighting.vhd
+++ fpga lighting.srcs/sources_1/new/lighting.vhd
@@ -22,22 +22,26 @@ architecture Behavioral of lighting is
     constant tp_clock: time := 1 sec / 125e6; -- Get time period, for 125MHz clock
     constant size: integer := 60*5;           -- Number of LEDs
     
-    type ram1 is array(0 to size*2) of std_logic_vector(0 to 23);
-    signal RAM_1 : ram1 := (others => "111111110000000011111111");
+    type ram is array(0 to size*2) of std_logic_vector(0 to 23); -- allocate BRAM for double buffering
+    signal vram : ram := (others => "111111110000000011111111"); -- initialise 
+    
     signal led_bits: std_logic_vector(0 to 23) := "000000000000000000000000"; -- each LED is 24 bits, we output the same bitstream to all LEDs
     
-    signal long: integer := time(0.4us) / tp_clock;         -- long duration, measured in clock pulses, 0.4us
-    signal short: integer := time(0.85us) / tp_clock;       -- short duration, measured in clock pulses, 0.85us
-    signal refresh: integer := time(50us) / tp_clock;     -- refresh duration, when strip is driven low, 50us 
+    signal long: integer := time(0.4us) / tp_clock;    -- long duration, measured in clock pulses, 0.4us
+    signal short: integer := time(0.85us) / tp_clock;  -- short duration, measured in clock pulses, 0.85us
+    signal refresh: integer := time(50us) / tp_clock;  -- refresh duration, when strip is driven low, 50us 
     
     signal clock_counter: integer := 0;    -- Counts clock pulses
     signal led_bit_counter: integer := 0;  -- Counts the Nth bit of an LED's colour data (24 bits total)
     signal led_counter: integer := 0;      -- LED number we're sending data for
-    signal spi_counter: integer := 0;
-    signal spi_frame_counter: integer := 0;
+    
+    signal spi_counter: integer := 0;         -- Counts the bit from SPI for the Nth bit of a single LED 
+    signal spi_frame_counter: integer := 0;   -- Data for the Nth LED
     signal spi_data: std_logic_vector(0 to 23) := "000000000000000000000000";
-    signal off: bit := '0';
-
+    
+    signal vram_part: bit := '0';            -- Writing SPI data, to first or second part of BRAM
+    signal vram_part_tmp: bit := '0';        -- mirrors above, for choosing where to read from BRAM
+    
     begin
         
         SPI: process(spi_clk)
@@ -47,16 +51,16 @@ architecture Behavioral of lighting is
                 if spi_counter = 23 then
                 
                     -- We have a double buffer, write to different part of BRAM
-                    if off = '0' then
-                        RAM_1(spi_frame_counter) <= spi_data(0 to 22) & spi_in  ;
+                    if vram_part = '0' then
+                        vram(spi_frame_counter) <= spi_data(0 to 22) & spi_in  ;
                     else
-                        RAM_1(spi_frame_counter+size) <= spi_data(0 to 22) & spi_in  ;
+                        vram(spi_frame_counter+size) <= spi_data(0 to 22) & spi_in  ;
                     end if;
                     
                     -- Check if we've reached end of data from SPI
                     if spi_frame_counter = size then
                         spi_frame_counter <= 0;
-                        off <= off xor '1';
+                        vram_part <= vram_part xor '1';
                     else
                         spi_frame_counter <= spi_frame_counter + 1; 
                     end if;
@@ -81,10 +85,10 @@ architecture Behavioral of lighting is
                 -- Counting clock edges
                 clock_counter <= clock_counter + 1;
             
-                if off = '0' then
-                    led_bits <= RAM_1(led_counter+size) ;
+                if vram_part_tmp = '0' then
+                    led_bits <= vram(led_counter+size) ;
                 else
-                    led_bits <= RAM_1(led_counter)  ;
+                    led_bits <= vram(led_counter)  ;
                 end if;
                                   
                 -- Check if we've reached end of LED strip
@@ -93,6 +97,7 @@ architecture Behavioral of lighting is
                     if clock_counter > refresh then
                         clock_counter <= 0;
                         led_counter <= 0;
+                        vram_part_tmp <= vram_part;
                     else
                         strip_1 <= '0';                    
                     end if;                
